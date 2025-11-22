3b2890046e319c3bb94b819c690b86abfcde7605
haseebshaik00 <haseebshaik00@gmail.com>
2025-11-21 20:39:52 -0800

    Initial commit from local

--- oops-practice.cpp
+++ oops-practice.cpp
@@ -2,22 +2,10 @@
 #include<iostream>
 using namespace std;
 
-class Person{
-public:
-    int id;
-    int age;
-    string name;
-    int population;
-};
-
-
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    Person p1;
-    Person p2(25, "Alice", 101);
     
 
     return 0;
