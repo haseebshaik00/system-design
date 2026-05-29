cc889248fa92d85cfb1b214c2f99ee4612dd6201
haseebshaik00 <haseebshaik00@gmail.com>
2026-05-29 15:32:22 -0700

    Add class & objects tips and tricks

--- 2-LLD/1-class-objects-tricks.cpp
+++ 2-LLD/1-class-objects-tricks.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <stdexcept>
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -14,8 +15,9 @@ private:
 public:
     BankAccount(const string& accountNumber, const string& ownerName):
     accountNumber(accountNumber), ownerName(ownerName), balance(0.0) {
-        // Initialize fields. Balance should start at 0.
-        // We cannot handle object creations like BankAccount("12") or BankAccount("Haseeb") using try-catch as these are compile-time errors and not run-time errors!
+        /* Initialize fields. Balance should start at 0.
+        We cannot handle object creations/constructor calls like BankAccount("12") or BankAccount("Haseeb") using try-catch as these are compile-time errors and not run-time errors! */
+        // Don't handle this by defining variable in initializer list - BankAccount(const string& accountNumber="Invalid", const string& ownerName="Guest") - this is wrong! If accountNumber and ownerName are compulsory - leave it as it is.
         if(accountNumber.empty())
             throw std::invalid_argument("Account Number cannot be empty!");
         if(ownerName.empty())
@@ -23,13 +25,14 @@ public:
     }
 
     void deposit(double amount) {
-        if(amount > 0) balance += amount;
+        if (amount > 0) balance += amount;
+        else throw invalid_argument("Deposit amount must be positive!");
     }
 
     bool withdraw(double amount) {
         // Remove amount from balance if sufficient funds exist
         // Return true if successful, false otherwise
-        if(amount <= balance){
+        if(amount>0 && amount <= balance){
             balance -= amount;
             return true;
         }   
@@ -39,7 +42,6 @@ public:
     double getBalance() const {
         // Return the current balance
         return balance;
-        return 0;
     }
 };
 
